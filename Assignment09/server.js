a8230d0ff71228c330b5baf5e2bfd30d1b3c8198
Pookan Pattaradanai <datas.pookan@gmail.com>
2024-02-28 18:55:16 +0700

    addAssignment09

--- Assignment09/server.js
+++ Assignment09/server.js
@@ -3,12 +3,19 @@ const dotenv = require('dotenv');
 const cookieParser=require('cookie-parser');
 const connectDB = require('./config/db');
 const mongoSanitize=require('express-mongo-sanitize');
+const helmet=require('helmet');
+const {xss}=require('express-xss-sanitizer');
+const rateLimit=require('express-rate-limit');
+const hpp=require('hpp');
+const cors=require('cors');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
 //Connect to database
 connectDB();
+
+const limiter=rateLimit({windowMs:10*60*1000,max:500});
 
 //Route files
 const hospitals = require('./routes/hospitals');
@@ -21,6 +28,11 @@ const app=express();
 app.use(express.json());
 
 app.use(mongoSanitize());
+app.use(helmet());
+app.use(xss());
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
 
 //Mount routers
 app.use('/api/v1/hospitals',hospitals);
